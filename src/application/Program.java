3d4a85c8ec33612d17018cddd194b76899c58fd1
Theylor Albuquerque <theylor18.albuquerque@gmail.com>
2023-04-25 20:28:07 -0300

    Primeira classe: Posicao

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,14 @@
 package application;
 
+import tabuleiro.Posicao;
+
 public class Program {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello World!");
+		Posicao pos = new Posicao(3, 5); // Instanciação de um objeto pos do tipo Posicao passando os valores 3 para linha e 5 para coluna.
+		
+		System.out.println(pos);
 
 	}
 
